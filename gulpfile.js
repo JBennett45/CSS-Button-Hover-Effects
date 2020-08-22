78c59f453220cb134681390ce9e2ab19da93a856
Jon Bennett <jon@boxedred.com>
2020-08-22 19:35:51 +0100

    initial setup and first few rows created Really rough and untested at the moment Styling needs work Move effects need adding SCSS will be split up towards the end - post codepen

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var elixir = require('laravel-elixir');
+
+var domain = 'http://testing.local/';
+
+var folderDir = 'dist/';
+
+elixir.config.assetsPath = folderDir
+elixir.config.publicPath = folderDir
+elixir.config.css.outputFolder = 'css';
+elixir.config.css.sass.folder = 'scss';
+
+elixir(function(mix) {
+	mix.sass('styles.scss');
+});
+
+
+elixir(function(mix) {
+	mix.browserSync({
+		open: false,
+		proxy: domain,
+		injectChanges: true,
+		files: [
+			folderDir+'/css/**/*.css',
+			folderDir+'/scss/sass/**/*.scss',
+			folderDir+'/js/**/*.js'
+		]
+	});
+});
